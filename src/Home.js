a83a49139aeccebce2e49554656f3ff52a1882c3
Hendrick L <h.lincertin@gmail.com>
2021-11-24 12:58:34 +0100

    feat: implement handling errors

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,31 @@ import Bloglist from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Sorry, could not fetch the data");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(err.message);
         setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
     </div>
