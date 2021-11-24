9d621d1cd86d4e997cca8ffe465ad766b06eb6eb
Hendrick L <h.lincertin@gmail.com>
2021-11-24 14:53:37 +0100

    feat: create usefetch custom hook to manage data, error, loadind

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,13 @@
 import { useState, useEffect } from "react";
 import Bloglist from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Sorry, could not fetch the data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        setError(err.message);
-        setIsLoading(false);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
