e7240d75e8f9266a0d5ec35c8325697db2e65307
Hendrick L <h.lincertin@gmail.com>
2021-11-30 21:10:18 +0100

    update db

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,19 @@
 const Create = () => {
   return (
     <div className="create">
-      <h2>Add a New Article</h2>
+      <h3>Add a New Article</h3>
+      <form>
+        <label>Title</label>
+        <input type="text" />
+        <label>Content</label>
+        <textarea cols="30" rows="10"></textarea>
+        <label>Author</label>
+        <select>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+          <option value="Princess">Princess</option>
+        </select>
+      </form>
     </div>
   );
 };
