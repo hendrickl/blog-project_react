5a46135e222c4cf602ae9701f14446242ffa8a0d
Hendrick L <h.lincertin@gmail.com>
2021-12-01 13:04:08 +0100

    feat: implement input state

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Yoshi");
+
   return (
     <div className="create">
       <h3>Add a New Article</h3>
       <form>
         <label>Title</label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Content</label>
-        <textarea cols="30" rows="10"></textarea>
+        <textarea
+          cols="30"
+          rows="10"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Author</label>
-        <select>
+        <select value={author} onChange={(e) => e.target.value}>
           <option value="Mario">Mario</option>
           <option value="Yoshi">Yoshi</option>
           <option value="Princess">Princess</option>
         </select>
       </form>
+      <p>{title}</p>
+      <p>{body}</p>
     </div>
   );
 };
